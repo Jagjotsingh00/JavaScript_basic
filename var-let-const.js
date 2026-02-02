df6b9cd8087e51876f9d9e36a4d356f1f69d4265
Jagjotsingh00 <iamjagjot02@gmail.com>
2026-02-02 14:22:57 +0530

    Let Var Const

---
+++ var-let-const.js
@@ -0,0 +1,61 @@
+// var a = 20;
+// console.log(a);
+// var a = 30;    // can be redeclare
+// console.log(a);
+
+// var a = 50;
+// console.log(a);
+// a = 90;    // can be assigned
+// console.log(a);
+
+
+
+// if (true){
+// var b = 20;
+// }
+// console.log(b); // global scope
+
+// ============================================================================
+
+// let e = 10;
+// let e = 20;   // error  //  cannot redeclare 
+
+
+
+
+//  let e = 30;
+//   e = 120 ;  // can be reassigned
+//   console.log(e);
+  
+
+
+//   if (true) {
+//   let  y = 20 ;
+//     console.log(y); // block scope
+//   }
+//   console.log(y); // let does not have global scope
+
+// console.log(o);
+// let o = 20;   // ReferenceError: Cannot access 'o' before initialization
+
+
+  
+// ==================================================================================
+
+
+// const v = 20 ;
+// const v = 30;   // error  //  cannot redeclare 
+
+
+//  const m = 30;
+//   m = 120 ;          // cannot be reassigned
+//   console.log(m);    // TypeError: Assignment to constant variable.
+  
+
+//   if (true) {
+//   const  s = 20 ;
+//     console.log(s); // block scope
+//   }
+//   console.log(s); // let does not have global scope      // ReferenceError: s is not defined
+
+// =====================================================================================-l,
