a7a6393035f5ebf333a2475bc03e1b33f3a0343d
Jagjotsingh00 <iamjagjot02@gmail.com>
2026-02-02 13:05:22 +0530

    Complete task related to switch statement and nested if else

--- js/nested-task.js
+++ js/nested-task.js
@@ -12,11 +12,101 @@
 
 // age >= 18 ? (hasLicence ? true : false) : console.log("You are too young to drive")
 
+// =================================================================================================================
 
 // const dbUsername = "Jagjot";
-// const dbPassword = "1234567";
-  
-// let username = qwert;
-// let password = 12345678;
+// const dbPassword = "123456";
 
-// if (username)
+// let username = "Jagjot";
+// let password = "123456";
+// // message = "Error"
+// if (username && password) {
+//   if (username === dbUsername) {
+//     if (password === dbPassword) {
+//       message = "You are LoggedIn";
+//     } else {
+//       message = "Enter valid Password";
+//     }
+//   } else {
+//     message = "Enter Valid Username";
+//   }
+// } else {
+//   message = "All Fields must be filled!";
+// }
+// document.write(message);
+
+//  =============================================================================================================
+
+// var username = "Jagjot";
+// var password = 123456;
+// // let message;
+// if (username && password) {
+//   if (String(username).length >= 5) {
+//     if (String(password).length >= 6) {
+//       message = "Account created";
+//     } else {
+//       message = "Password Minimum length is 6";
+//     }
+//   } else {
+//     message = "Username Minimum length is 5";
+//   }
+// } else {
+//   message = "All Fields must be filled!";
+// }
+
+// document.write(message);
+
+// ==============================================================================================================
+
+// var day = "monday";
+// let message;
+// if (day === "monday") {
+//   message = "TODAY IS MONDAY";
+// } else if (day === "tuesday") {
+//    message = "TODAY IS TUESDAY";
+// } else if (day === "wednesday") {
+// message = "TODAY IS WEDNESDAY";
+// } else if ((day ===  "thusday")) {
+//   message = "TODAY IS THUSDAY";
+// } else if (day === "friday") {
+//   message = "TODAY IS FRIDAY";
+// } else if (day === "saturday") {
+//   message = "TODAY IS SATURDAY";
+// } else if (day === "sunday") {
+//   message = "TODAY IS SUNDAY";
+// } else message = "Enter valid Data";
+
+// document.write(message);
+
+// =============================================================================================================
+
+// let day = "Tuesday";
+
+// switch (day) {
+//   case "monday":
+//     message = "Today is Monday!";
+//     break;
+//   case "Tuesday":
+//     message = "Today is Tuesday!";
+//     break;
+//   case "Wednesday":
+//     message = "Today is Wednesday!";
+//     break; 
+//   case "Thusday":
+//     message = "Today is Thusday";
+//     break;
+//   case "Friday":
+//     message = "Today is friday";
+//     break;
+//   case "Saturday":
+//     message = "Today is Saturday";
+//     break;
+//   case "Sunday":
+//     message = "Today is Sunday";
+//     break;
+
+//   default:
+//     message = "Please Enter Valid Data";
+//     break;
+// }
+// document.write(message);
